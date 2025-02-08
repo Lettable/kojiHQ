3a971adeab9721c2dceb08c1df5bef9cdf56e1d7
Mirza <devmirzaofficial@gmail.com>
2025-02-08 16:32:51 +0500

    choar(font): chnaged font to kanit

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,17 +5,6 @@ import Head from 'next/head';
 import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react"
 
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 const kanit = Kanit({
   subsets: ["latin"],
   variable: "--font-kanit",
