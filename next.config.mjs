eb98b862cc88226caef10e1d87725fd32e601b2b
Mirza <devmirzaofficial@gmail.com>
2025-02-17 18:47:11 +0500

    feat(activity): activity feed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.ibb.co', 'pbs.twimg.com', 'images.unsplash.com', 'imgur.com', 's13.gifyu.com', 'i.imgur.com', 'i.postimg.cc'],
+    domains: ['i.ibb.co', 'pbs.twimg.com', 'images.unsplash.com', 'imgur.com', 's13.gifyu.com', 'i.imgur.com', 'i.postimg.cc', 'encrypted-tbn0.gstatic.com'],
   }
 };
 
