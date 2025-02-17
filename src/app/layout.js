c4e2142673a6f5dd6ccb50cd6dfc1da704977560
Mirza <devmirzaofficial@gmail.com>
2025-02-17 08:16:57 +0500

    feat(activity): activity feed

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,19 +12,19 @@ const kanit = Kanit({
 });
 
 export const metadata = {
-  "title": "Koji - Buy, Sell & Trade Digital Goods",
-  "description": "Koji is your go-to platform for buying, selling, and trading digital goods, services, and exclusive content. Connect with a thriving community, find unique deals, and turn your skills into profit.",
-  "keywords": "marketplace, forum, buy, sell, trade, digital goods, services, community, Koji",
-  "author": "Mirzya",
+  "title": "Suized - Seized, but Never Silenced",
+  "description": "Suized is your go-to platform for buying, selling, and trading digital goods, services, and exclusive content. Connect with a thriving community, find unique deals, and turn your skills into profit.",
+  "keywords": "marketplace, forum, buy, sell, trade, digital goods, services, community, Suized",
+  "author": "Anon",
   "openGraph": {
-    "title": "Koji - Buy, Sell & Trade Digital Goods",
+    "title": "Suized - Seized, but Never Silenced",
     "description": "Discover and trade digital goods and services on Koji Marketplace. Connect with buyers and sellers in a secure, thriving community.",
     "type": "website"
   },
   "telegram": {
     "site": "@",
-    "title": "Koji Marketplace - Buy, Sell & Trade",
-    "description": "Join Koji Marketplace to buy, sell, and trade digital goods and services securely."
+    "title": "Suized - Seized, but Never Silenced",
+    "description": "Join Suized to buy, sell, and trade digital goods and services securely."
   }
 }
 
@@ -34,7 +34,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <meta name="cryptomus" content="4884514a" />
-        
       </Head>
       <body
         className={`${kanit.className} antialiased`}
